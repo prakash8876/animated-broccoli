702cdc472221c8a5f4325c2220728c639d99f1e4
Prakash <prakash8876@gmail.com>
2023-12-23 01:44:39 +0530

    iniate commit

---
+++ ms-office-department/src/main/java/com/matoshri/msofficedepartment/MsOfficeDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.matoshri.msofficedepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOfficeDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOfficeDepartmentApplication.class, args);
+	}
+
+}
