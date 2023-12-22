71555afb026ea049764c6d6e94d439ead1a44ac2
Prakash <prakash8876@gmail.com>
2023-12-23 03:08:25 +0530

    update #4

---
+++ ms-office-employee/src/test/java/com/matoshri/employee/MsOfficeEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matoshri.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOfficeEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
