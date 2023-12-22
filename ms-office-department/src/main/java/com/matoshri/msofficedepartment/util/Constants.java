8c493e2121cd74d49df850e57917697882320ca1
Prakash <prakash8876@gmail.com>
2023-12-23 01:53:54 +0530

    base setup

---
+++ ms-office-department/src/main/java/com/matoshri/msofficedepartment/util/Constants.java
@@ -0,0 +1,8 @@
+package com.matoshri.msofficedepartment.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Constants {
+    private Constants() {}
+}
---
+++ ms-office-employee/src/main/java/com/matoshri/employee/util/Constants.java
@@ -0,0 +1,8 @@
+package com.matoshri.employee.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Constants {
+    private Constants() {}
+}
